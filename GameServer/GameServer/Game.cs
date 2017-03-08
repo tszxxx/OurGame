6bb29b32d7c268b035fe39d90a581de891edd98a
tszxxx <zhenghangjie@126.com>
2017-03-08 20:43:09 +0800

    服务器

---
+++ GameServer/GameServer/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Game
+    {
+        private Server myServer;
+        public void Init()
+        {
+            Server myServer = new Server();
+        }
+    }
+}
