eba4d71a406d87ed0f0387e3f1974b23caedbef2
tszxxx <zhenghangjie@126.com>
2017-03-10 21:32:45 +0800

    客户端

--- 客户端/GameClient/GameClient/Client.cs
+++ 客户端/GameClient/GameClient/Client.cs
@@ -14,8 +14,9 @@ namespace GameClient
     {
         private static UdpClient UdpClient;
         private static Thread sendThread, recvThread;
-        private static Semaphore mySem = new Semaphore(0, 1);
+        private static Semaphore mySendSem, myPlayerSem;
         private static string Ipaddr;
+        private static Player[] myPlayers;
         public string IPAddr {
             get { return Ipaddr; }
             set { Ipaddr = value; }
@@ -33,11 +34,11 @@ namespace GameClient
         {
             while (true)
             {
-                mySem.WaitOne();
+                mySendSem.WaitOne();
                 try
                 {
                     Byte[] SendBytes;
-                    SendBytes = new Byte[100];
+                    SendBytes = new Byte[240];
                     for (int i = 0; i < SendBytes.Length; i++)
                         SendBytes[i] = (byte) '1';
                     UdpClient.Connect(Ipaddr, port);
@@ -60,6 +61,7 @@ namespace GameClient
                     IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Parse(Ipaddr), 11000);
                     Byte[] receiveBytes = Client.UdpClient.Receive(ref RemoteIpEndPoint);
                     MessageBox.Show(receiveBytes.ToString());
+                    RecvPlayers(receiveBytes);
                 }
                 catch (Exception e)
                 {
@@ -70,13 +72,38 @@ namespace GameClient
         }
         public static void Begin()
         {
+            mySendSem = new Semaphore(0, 1);
+            myPlayerSem = new Semaphore(0, 1);
+            myPlayers = new Player[10];
             Ipaddr = "10.211.55.30";
             UdpClient = new UdpClient(port);
-            mySem.Release();
+            mySendSem.Release();
             sendThread = new Thread(new ThreadStart(SendFunc));
             recvThread = new Thread(new ThreadStart(RecvFunc));
             sendThread.Start();
             recvThread.Start();
+        }
+        private static void RecvPlayers(Byte[] RecvBytes)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                myPlayers[i].HP = (RecvBytes[0 + 32 * i] << 24) + (RecvBytes[1 + 32 * i] << 16) + (RecvBytes[2 + 32 * i] << 8) + RecvBytes[3 + 32 * i];
+                myPlayers[i].MP = (RecvBytes[4 + 32 * i] << 24) + (RecvBytes[5 + 32 * i] << 16) + (RecvBytes[6 + 32 * i] << 8) + RecvBytes[7 + 32 * i];
+                myPlayers[i].PD = (RecvBytes[8 + 32 * i] << 24) + (RecvBytes[9 + 32 * i] << 16) + (RecvBytes[10 + 32 * i] << 8) + RecvBytes[11 + 32 * i];
+                myPlayers[i].MD = (RecvBytes[12 + 32 * i] << 24) + (RecvBytes[13 + 32 * i] << 16) + (RecvBytes[14 + 32 * i] << 8) + RecvBytes[15 + 32 * i];
+                myPlayers[i].PR = (RecvBytes[16 + 32 * i] << 24) + (RecvBytes[17 + 32 * i] << 16) + (RecvBytes[18 + 32 * i] << 8) + RecvBytes[19 + 32 * i];
+                myPlayers[i].MR = (RecvBytes[20 + 32 * i] << 24) + (RecvBytes[21 + 32 * i] << 16) + (RecvBytes[22 + 32 * i] << 8) + RecvBytes[23 + 32 * i];
+                myPlayers[i].X = (RecvBytes[24 + 32 * i] << 24) + (RecvBytes[25 + 32 * i] << 16) + (RecvBytes[26 + 32 * i] << 8) + RecvBytes[27 + 32 * i];
+                myPlayers[i].Y = (RecvBytes[28 + 32 * i] << 24) + (RecvBytes[29 + 32 * i] << 16) + (RecvBytes[30 + 32 * i] << 8) + RecvBytes[31 + 32 * i];
+            }
+        }
+        private static Byte[] SendPlayers()
+        {
+            Byte[] SendBytes = new byte[320];
+            for (int i = 0; i < 10; i++)
+            {
+            }
+            return SendBytes;
         }
     }
 }
