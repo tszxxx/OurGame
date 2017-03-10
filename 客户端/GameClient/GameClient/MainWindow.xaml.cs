40041a83765c31a707315db889a87c8b33730e85
tszxxx <zhenghangjie@126.com>
2017-03-10 22:10:59 +0800

    客户端——初级完整版

---
+++ 客户端/GameClient/GameClient/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Client myClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myClient = new Client();
+            Thread MainThread = new Thread(new ThreadStart(Client.Begin));
+            MainThread.Start();
+        }
+
+        private void onClick(object sender, RoutedEventArgs e)
+        {
+
+            myClient.IPAddr = myTextBox.Text.ToString();
+        }
+    }
+}
