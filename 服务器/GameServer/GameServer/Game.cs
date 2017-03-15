2ecb07f9f5872de181d7b734a810adf0d978e0b7
tszxxx <zhenghangjie@126.com>
2017-03-15 09:21:53 +0800

    刚发错了,客户端unity版本

---
+++ 服务器/GameServer/GameServer/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Game
+    {
+        private Server myServer;
+        public string Init()
+        {
+            Server myServer = new Server();
+            myServer.Begin();
+            return myServer.GetAddressIP();
+        }
+
+    }
+}
